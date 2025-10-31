319d761607822214f9c6c7cca8b099d8141c7c94
sanketkumbharstu@gmail.com <120247634+SNKT2412@users.noreply.github.com>
2025-11-01 01:50:00 +0530

    Final Commit

---
+++ config/FirebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "meeting-scheduler-3414c.firebaseapp.com",
+  projectId: "meeting-scheduler-3414c",
+  storageBucket: "meeting-scheduler-3414c.firebasestorage.app",
+  messagingSenderId: "1077191117677",
+  appId: "1:1077191117677:web:be566cf2f081b385cca864",
+  measurementId: "G-EX84PHVV4W",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
